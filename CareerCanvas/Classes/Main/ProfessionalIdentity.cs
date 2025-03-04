41dc50d68ebd4ce1ffcd79dda53ff9445d11b2f8
Branden Stober <brandenstober@outlook.com>
2025-03-03 23:54:26 -0500

    Added identity class

---
+++ CareerCanvas/Classes/Main/ProfessionalIdentity.cs
@@ -0,0 +1,41 @@
+﻿using ProtoBuf;
+
+namespace CareerCanvas.Classes.Main
+{
+    [ProtoContract]
+    public class ProfessionalIdentity
+    {
+        [ProtoMember(1)]
+        public string FirstName { get; set; } = String.Empty;
+
+        [ProtoMember(2)]
+        public string MiddleName { get; set; } = String.Empty;
+
+        [ProtoMember(3)]
+        public string LastName { get; set; } = String.Empty;
+
+        [ProtoMember(4)]
+        public string Address { get; set; } = String.Empty;
+
+        [ProtoMember(5)]
+        public string City { get; set; } = String.Empty;
+
+        [ProtoMember(6)]
+        public string State { get; set; } = String.Empty;
+
+        [ProtoMember(7)]
+        public string ZipCode { get; set; } = String.Empty;
+
+        [ProtoMember(8)]
+        public string PhoneNumber { get; set; } = String.Empty;
+
+        [ProtoMember(9)]
+        public string? Email { get; set; } = String.Empty;
+
+        [ProtoMember(10)]
+        public string? LinkedIn { get; set; } = String.Empty;
+
+        [ProtoMember(11)]
+        public string? Portfolio { get; set; } = String.Empty;
+    }
+}
--- CareerCanvas/Forms/EntryForm.Designer.cs
+++ CareerCanvas/Forms/EntryForm.Designer.cs
@@ -32,10 +32,15 @@
             welcomePage = new TabPage();
             readMeView = new Microsoft.Web.WebView2.WinForms.WebView2();
             identityPage = new TabPage();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            identitiesLabel = new ReaLTaiizor.Controls.MaterialLabel();
+            identitiesListBox = new ReaLTaiizor.Controls.MaterialListBox();
+            identityButtonsFlow = new FlowLayoutPanel();
+            newIdentityButton = new ReaLTaiizor.Controls.MaterialButton();
             resumePage = new TabPage();
-            recentsFlowPanel = new FlowLayoutPanel();
-            recentsLabel = new ReaLTaiizor.Controls.MaterialLabel();
             recentResumesFlow = new FlowLayoutPanel();
+            recentResumesLabel = new ReaLTaiizor.Controls.MaterialLabel();
+            recentResumesListBox = new ReaLTaiizor.Controls.MaterialListBox();
             mainMenuButtonFlowPanel = new FlowLayoutPanel();
             newResumeButton = new ReaLTaiizor.Controls.MaterialButton();
             loadResumeButton = new ReaLTaiizor.Controls.MaterialButton();
@@ -44,8 +49,11 @@
             materialTabControl1.SuspendLayout();
             welcomePage.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)readMeView).BeginInit();
+            identityPage.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            identityButtonsFlow.SuspendLayout();
             resumePage.SuspendLayout();
-            recentsFlowPanel.SuspendLayout();
+            recentResumesFlow.SuspendLayout();
             mainMenuButtonFlowPanel.SuspendLayout();
             SuspendLayout();
             // 
@@ -91,6 +99,8 @@
             // 
             // identityPage
             // 
+            identityPage.Controls.Add(flowLayoutPanel1);
+            identityPage.Controls.Add(identityButtonsFlow);
             identityPage.Location = new Point(4, 24);
             identityPage.Name = "identityPage";
             identityPage.Size = new Size(1274, 570);
@@ -98,9 +108,80 @@
             identityPage.Text = "Identities";
             identityPage.UseVisualStyleBackColor = true;
             // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(identitiesLabel);
+            flowLayoutPanel1.Controls.Add(identitiesListBox);
+            flowLayoutPanel1.Dock = DockStyle.Right;
+            flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel1.Location = new Point(924, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(350, 570);
+            flowLayoutPanel1.TabIndex = 3;
+            // 
+            // identitiesLabel
+            // 
+            identitiesLabel.Anchor = AnchorStyles.Top;
+            identitiesLabel.Depth = 0;
+            identitiesLabel.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            identitiesLabel.Location = new Point(3, 0);
+            identitiesLabel.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.HOVER;
+            identitiesLabel.Name = "identitiesLabel";
+            identitiesLabel.Size = new Size(340, 23);
+            identitiesLabel.TabIndex = 4;
+            identitiesLabel.Text = "Identities";
+            identitiesLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // identitiesListBox
+            // 
+            identitiesListBox.BackColor = Color.White;
+            identitiesListBox.BorderColor = Color.LightGray;
+            identitiesListBox.Depth = 0;
+            identitiesListBox.Dock = DockStyle.Bottom;
+            identitiesListBox.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            identitiesListBox.Location = new Point(6, 29);
+            identitiesListBox.Margin = new Padding(6);
+            identitiesListBox.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.HOVER;
+            identitiesListBox.Name = "identitiesListBox";
+            identitiesListBox.SelectedIndex = -1;
+            identitiesListBox.SelectedItem = null;
+            identitiesListBox.Size = new Size(334, 535);
+            identitiesListBox.TabIndex = 2;
+            // 
+            // identityButtonsFlow
+            // 
+            identityButtonsFlow.Controls.Add(newIdentityButton);
+            identityButtonsFlow.Dock = DockStyle.Left;
+            identityButtonsFlow.Location = new Point(0, 0);
+            identityButtonsFlow.Name = "identityButtonsFlow";
+            identityButtonsFlow.Size = new Size(158, 570);
+            identityButtonsFlow.TabIndex = 0;
+            // 
+            // newIdentityButton
+            // 
+            newIdentityButton.Anchor = AnchorStyles.None;
+            newIdentityButton.AutoSize = false;
+            newIdentityButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            newIdentityButton.Density = ReaLTaiizor.Controls.MaterialButton.MaterialButtonDensity.Default;
+            newIdentityButton.Depth = 0;
+            newIdentityButton.HighEmphasis = true;
+            newIdentityButton.Icon = null;
+            newIdentityButton.IconType = ReaLTaiizor.Controls.MaterialButton.MaterialIconType.Rebase;
+            newIdentityButton.Location = new Point(6, 6);
+            newIdentityButton.Margin = new Padding(6);
+            newIdentityButton.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.HOVER;
+            newIdentityButton.Name = "newIdentityButton";
+            newIdentityButton.NoAccentTextColor = Color.Empty;
+            newIdentityButton.Size = new Size(136, 36);
+            newIdentityButton.TabIndex = 1;
+            newIdentityButton.Text = "New Identity";
+            newIdentityButton.Type = ReaLTaiizor.Controls.MaterialButton.MaterialButtonType.Contained;
+            newIdentityButton.UseAccentColor = false;
+            newIdentityButton.UseVisualStyleBackColor = true;
+            // 
             // resumePage
             // 
-            resumePage.Controls.Add(recentsFlowPanel);
+            resumePage.Controls.Add(recentResumesFlow);
             resumePage.Controls.Add(mainMenuButtonFlowPanel);
             resumePage.Location = new Point(4, 24);
             resumePage.Name = "resumePage";
@@ -109,45 +190,53 @@
             resumePage.Text = "Resume";
             resumePage.UseVisualStyleBackColor = true;
             // 
-            // recentsFlowPanel
-            // 
-            recentsFlowPanel.Controls.Add(recentsLabel);
-            recentsFlowPanel.Controls.Add(recentResumesFlow);
-            recentsFlowPanel.FlowDirection = FlowDirection.TopDown;
-            recentsFlowPanel.Location = new Point(759, 3);
-            recentsFlowPanel.Name = "recentsFlowPanel";
-            recentsFlowPanel.Size = new Size(512, 564);
-            recentsFlowPanel.TabIndex = 3;
-            // 
-            // recentsLabel
-            // 
-            recentsLabel.Anchor = AnchorStyles.Top;
-            recentsLabel.Depth = 0;
-            recentsLabel.Font = new Font("Roboto", 24F, FontStyle.Bold, GraphicsUnit.Pixel);
-            recentsLabel.FontType = ReaLTaiizor.Manager.MaterialSkinManager.FontType.H5;
-            recentsLabel.HighEmphasis = true;
-            recentsLabel.Location = new Point(3, 0);
-            recentsLabel.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.HOVER;
-            recentsLabel.Name = "recentsLabel";
-            recentsLabel.Size = new Size(503, 29);
-            recentsLabel.TabIndex = 3;
-            recentsLabel.Text = "Recent Resumes";
-            recentsLabel.TextAlign = ContentAlignment.BottomCenter;
-            // 
             // recentResumesFlow
             // 
-            recentResumesFlow.Dock = DockStyle.Bottom;
-            recentResumesFlow.Location = new Point(3, 32);
+            recentResumesFlow.Controls.Add(recentResumesLabel);
+            recentResumesFlow.Controls.Add(recentResumesListBox);
+            recentResumesFlow.Dock = DockStyle.Right;
+            recentResumesFlow.FlowDirection = FlowDirection.TopDown;
+            recentResumesFlow.Location = new Point(928, 0);
             recentResumesFlow.Name = "recentResumesFlow";
-            recentResumesFlow.Size = new Size(503, 527);
-            recentResumesFlow.TabIndex = 4;
+            recentResumesFlow.Size = new Size(346, 570);
+            recentResumesFlow.TabIndex = 2;
+            // 
+            // recentResumesLabel
+            // 
+            recentResumesLabel.Anchor = AnchorStyles.Top;
+            recentResumesLabel.Depth = 0;
+            recentResumesLabel.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            recentResumesLabel.Location = new Point(3, 0);
+            recentResumesLabel.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.HOVER;
+            recentResumesLabel.Name = "recentResumesLabel";
+            recentResumesLabel.Size = new Size(340, 23);
+            recentResumesLabel.TabIndex = 0;
+            recentResumesLabel.Text = "Recent Resumes";
+            recentResumesLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // recentResumesListBox
+            // 
+            recentResumesListBox.BackColor = Color.White;
+            recentResumesListBox.BorderColor = Color.LightGray;
+            recentResumesListBox.Depth = 0;
+            recentResumesListBox.Dock = DockStyle.Bottom;
+            recentResumesListBox.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            recentResumesListBox.Location = new Point(6, 29);
+            recentResumesListBox.Margin = new Padding(6);
+            recentResumesListBox.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.HOVER;
+            recentResumesListBox.Name = "recentResumesListBox";
+            recentResumesListBox.SelectedIndex = -1;
+            recentResumesListBox.SelectedItem = null;
+            recentResumesListBox.Size = new Size(334, 534);
+            recentResumesListBox.TabIndex = 1;
             // 
             // mainMenuButtonFlowPanel
             // 
             mainMenuButtonFlowPanel.Controls.Add(newResumeButton);
             mainMenuButtonFlowPanel.Controls.Add(loadResumeButton);
+            mainMenuButtonFlowPanel.Dock = DockStyle.Left;
             mainMenuButtonFlowPanel.FlowDirection = FlowDirection.TopDown;
-            mainMenuButtonFlowPanel.Location = new Point(3, 3);
+            mainMenuButtonFlowPanel.Location = new Point(0, 0);
             mainMenuButtonFlowPanel.Name = "mainMenuButtonFlowPanel";
             mainMenuButtonFlowPanel.Size = new Size(181, 570);
             mainMenuButtonFlowPanel.TabIndex = 1;
@@ -228,8 +317,11 @@
             materialTabControl1.ResumeLayout(false);
             welcomePage.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)readMeView).EndInit();
+            identityPage.ResumeLayout(false);
+            flowLayoutPanel1.ResumeLayout(false);
+            identityButtonsFlow.ResumeLayout(false);
             resumePage.ResumeLayout(false);
-            recentsFlowPanel.ResumeLayout(false);
+            recentResumesFlow.ResumeLayout(false);
             mainMenuButtonFlowPanel.ResumeLayout(false);
             ResumeLayout(false);
         }
@@ -242,12 +334,17 @@
         private FlowLayoutPanel mainMenuButtonFlowPanel;
         private ReaLTaiizor.Controls.MaterialButton newResumeButton;
         private ReaLTaiizor.Controls.MaterialButton loadResumeButton;
-        private FlowLayoutPanel recentsFlowPanel;
-        private ReaLTaiizor.Controls.MaterialLabel recentsLabel;
-        private FlowLayoutPanel recentResumesFlow;
         private TabPage welcomePage;
         private TabPage cvPage;
         private TabPage identityPage;
         private Microsoft.Web.WebView2.WinForms.WebView2 readMeView;
+        private FlowLayoutPanel identityButtonsFlow;
+        private ReaLTaiizor.Controls.MaterialButton newIdentityButton;
+        private ReaLTaiizor.Controls.MaterialListBox identitiesListBox;
+        private FlowLayoutPanel recentResumesFlow;
+        private ReaLTaiizor.Controls.MaterialLabel recentResumesLabel;
+        private ReaLTaiizor.Controls.MaterialListBox recentResumesListBox;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private ReaLTaiizor.Controls.MaterialLabel identitiesLabel;
     }
 }
