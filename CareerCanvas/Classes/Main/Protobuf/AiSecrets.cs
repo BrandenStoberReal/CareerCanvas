bbf1f29c8f6ff46125614675f698c16118890bed
Branden Stober <brandenstober@outlook.com>
2025-06-17 21:17:11 -0400

    Test

--- CareerCanvas/Classes/Main/Protobuf/AiSecrets.cs
+++ CareerCanvas/Classes/Main/Protobuf/AiSecrets.cs
@@ -1,4 +1,5 @@
-﻿using ProtoBuf;
+﻿using LlmTornado.Code;
+using ProtoBuf;
 
 namespace CareerCanvas.Classes.Main.Protobuf
 {
@@ -6,10 +7,6 @@ namespace CareerCanvas.Classes.Main.Protobuf
     public class AiSecrets
     {
         [ProtoMember(1)]
-        public string OpenAiApiKey { get; set; } = string.Empty;
-        [ProtoMember(2)]
-        public string AnthropicApiKey { get; set; } = string.Empty;
-        [ProtoMember(3)]
-        public string GoogleApiKey { get; set; } = string.Empty;
+        public List<Tuple<LLmProviders, string>> ApiKeys { get; set; } = new List<Tuple<LLmProviders, string>>();
     }
 }
---
+++ CareerCanvas/Classes/Main/Protobuf/Preferences.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LlmTornado;
+using LlmTornado.Code;
+
+namespace CareerCanvas.Classes.Main.Protobuf
+{
+    public class Preferences
+    {
+        public LLmProviders LlmProvider { get; set; } = LLmProviders.Unknown;
+    }
+}
